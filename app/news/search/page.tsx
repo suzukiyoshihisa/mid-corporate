e54867d9c0aabda639602d33bc6318739c3f6292
suzukiyoshihisa <mf37459@gmail.com>
2025-02-21 04:07:28 +0900

    page修正

--- app/news/search/page.tsx
+++ app/news/search/page.tsx
@@ -1,24 +1,19 @@
 import { getNewsList } from '@/app/_libs/microcms';
-import { NEWS_LIST_LIMIT } from '@/app/_constants';
 import NewsList from '@/app/_components/NewsList';
+import Pagination from '@/app/_components/Pagination';
 import SearchField from '@/app/_components/SearchField';
+import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
-type Props = {
-  searchParams: {
-    q?: string;
-  };
-};
-
-export default async function Page({ searchParams }: Props) {
-  const { contents: news } = await getNewsList({
+export default async function Page() {
+  const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
-    q: searchParams.q,
   });
 
   return (
     <>
       <SearchField />
       <NewsList news={news} />
+      <Pagination totalCount={totalCount} />
     </>
   );
 }
