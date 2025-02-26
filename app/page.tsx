7357637141e0c8ecaa302f17ca26c0d90a5fc0a2
suzukiyoshihisa <mf37459@gmail.com>
2025-02-26 21:04:46 +0900

    priority

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default async function Home() {
           alt=""
           width={4000}
           height={1200}
+          priority
         />
       </section>
       <section className={styles.news}>
