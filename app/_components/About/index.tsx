a10276b0f2576a7a19460c50d387e0e59b0360a7
suzukiyoshihisa <mf37459@gmail.com>
2025-05-26 16:49:38 +0900

    Aboutの調整

--- app/_components/About/index.tsx
+++ app/_components/About/index.tsx
@@ -42,15 +42,17 @@ export default function About() {
     <div className={styles.inner} id="about">
       <div className={styles.container}>
         <AnimatedContent delay={0}>
-          <h2 className={styles.title}>About Us.</h2>
-          <p className={styles.text}>
-            株式会社mid（ミッド）は、新宿に拠点を置くクリエイティブカンパニー。<br />
-            自社ブランド「WANSIE（ワンジー）」の企画・製造をはじめ、WEB制作やグラフィックデザイン、人材派遣など、領域を横断した事業を展開しています。
-          </p>
-          <p className={styles.text}>
-            私たちが大切にしているのは、表面的なトレンドに流されることなく、個人や企業が持つ本来の魅力や事業の本質に目を向けること。
-            長く愛される価値を築くために、丁寧に言葉を交わしながら、その核を形にしていきます。
-          </p>
+          <div className={styles.textContainer}>
+            <h2 className={styles.title}>About Us.</h2>
+            <p className={styles.text}>
+              株式会社mid（ミッド）は、新宿に拠点を置くクリエイティブカンパニー。<br />
+              自社ブランド「WANSIE（ワンジー）」の企画・製造をはじめ、WEB制作やグラフィックデザイン、人材派遣など、領域を横断した事業を展開しています。
+            </p>
+            <p className={styles.text}>
+              私たちが大切にしているのは、表面的なトレンドに流されることなく、個人や企業が持つ本来の魅力や事業の本質に目を向けること。
+              長く愛される価値を築くために、丁寧に言葉を交わしながら、その核を形にしていきます。
+            </p>
+          </div>
         </AnimatedContent>
       </div>
 
